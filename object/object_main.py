f738bdfe662aa1c5efa75586a216f8f115eb35bc
Hyun Jin Choi <calvinhyun.choi@mail.utoronto.ca>
2018-07-14 15:54:15 -0400

    Initial commit. All files in working order.

---
+++ object/object_main.py
@@ -0,0 +1,21 @@
+import boto3
+
+questions = open('object_qs.xml').read()
+answers = open('object_ans').read()
+
+mturk = boto3.client('mturk',
+                     region_name='us-east-1',
+                     endpoint_url='https://mturk-requester-sandbox.us-east-1.amazonaws.com')
+
+qual_response = mturk.create_qualification_type(
+    Name='Duncan Lab Test',
+    Description='This is a qualification test required for participation in the Duncan task.',
+    Keywords='test,qualification,duncan',
+    QualificationTypeStatus='Active',
+    Test=questions,
+    AnswerKey=answers,
+    TestDurationInSeconds=300)
+
+# The QUALIFICATION_TYPE_ID required to use this qualification in a HIT
+# This can also be found on the MTurk website in the page of this created qualification
+# print(qual_response['QualificationType']['QualificationTypeId'])
